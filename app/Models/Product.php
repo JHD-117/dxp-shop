facf285ffc2d11d99af87e98007e90e2325d8c4d
JHD-117 <dubervalderrama8@gmail.com>
2026-01-06 18:22:46 -0500

    feat(app)Primer commit

---
+++ app/Models/Product.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
+use App\Models\Brand;
+use App\Models\OrderItem;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'slug',
+        'images',
+        'description',
+        'price',
+        'is_active',
+        'is_featured',
+        'in_stock',
+        'on_sale',
+        'category_id',
+        'brand_id',
+        'reference_brand_id',
+    ];
+
+    protected $casts = [
+        'images' => 'array',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function reference()
+    {
+        return $this->belongsTo(Reference::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
