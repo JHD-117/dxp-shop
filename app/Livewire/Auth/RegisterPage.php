c6bcfdf061e0e3de8ded14005947b9b17fd6ab38
JHD-117 <dubervalderrama8@gmail.com>
2026-01-27 22:48:17 -0500

    feat(register)Agrega logica para registro

--- app/Livewire/Auth/RegisterPage.php
+++ app/Livewire/Auth/RegisterPage.php
@@ -3,11 +3,35 @@
 namespace App\Livewire\Auth;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
+use App\Models\User;
 
+#[Title('Register')]
 class RegisterPage extends Component
 {
+    public $name, $email, $password;
+
     public function render()
     {
         return view('livewire.auth.register-page');
+    }
+
+    public function save()
+    {
+        $this->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => bcrypt($this->password),
+        ]);
+
+        auth()->login($user);
+
+        return redirect()->intended();
     }
 }
